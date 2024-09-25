d38c9851646bcfef84b957b3276bcd7a227e3d3d
Jérémy Colombet <jeremy.colombet@garganttua.com>
2024-09-25 21:38:45 +0200

    give access to the engine through the spring bean

--- src/main/java/com/garganttua/events/spring/IGGEventsEngineSpringBean.java
+++ src/main/java/com/garganttua/events/spring/IGGEventsEngineSpringBean.java
@@ -1,6 +1,7 @@
 package com.garganttua.events.spring;
 
 import com.garganttua.events.spec.exceptions.GGEventsException;
+import com.garganttua.events.spec.interfaces.IGGEventsEngine;
 
 public interface IGGEventsEngineSpringBean {
 
@@ -13,5 +14,7 @@ public interface IGGEventsEngineSpringBean {
 	void init() throws GGEventsException;
 	
 	void flush() throws GGEventsException;
+	
+	IGGEventsEngine getEngine();
 
 }
